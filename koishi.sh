8a8c1471df88f2977be269ea2aaa2891f62857ca
faangyi <faangyi@hotmail.com>
2025-08-08 07:43:35 +0800

    Update koishi.sh

--- koishi.sh
+++ koishi.sh
@@ -74,8 +74,8 @@ nohup /home/user/$username/cloudflared/cloudflared tunnel --no-autoupdate --edge
 # 安装koishi
 cd /home/user/$username
 npm init -y koishi@latest koishi-app -- --yes
-echo "正在安装koishi，请耐心等待"
 cd koishi-app
+echo "正在安装koishi，请耐心等待"
 npm install
 
 # 修改默认启动后打开webui
